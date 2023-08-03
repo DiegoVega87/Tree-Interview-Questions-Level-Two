461540a99feb277de170cdbdd924eb42797438ce
Diego Vega <diegovega@me.com>
2023-08-03 17:55:49 +0200

    Add examples

--- src/TreeTraversal2.java
+++ src/TreeTraversal2.java
@@ -7,5 +7,33 @@ public class TreeTraversal2 {
     * order traversal of its nodes' values. This means that the traversal should go from left to right for the first
     * level, then from right to left for the second level, and so on.
     *
-     * */
+    * Example 1:
+    *  Input:
+    *     3
+    *    / \
+    *   9  20
+    *     /  \
+    *    15   7
+    *   Output: [3, 20, 9, 15, 7]
+    *   Explanation:
+    *   The zigzag level order traversal goes as follows:
+    *   Level 1: 3
+    *   Level 2: 20 -> 9
+    *   Level 3: 15 -> 7
+    *
+    * Example 2:
+    *       Input:
+    *     1
+    *    / \
+    *   2   3
+    *  / \   \
+    * 4   5   6
+    *
+    * Output: [1, 3, 2, 4, 5, 6]
+    *   Explanation:
+    *   The zigzag level order traversal goes as follows:
+    *   Level 1: 1
+    *   Level 2: 3 -> 2
+    *   Level 3: 4 -> 5 -> 6
+    * */
 }
