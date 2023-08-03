0d8c47220ade9715fba207be49264ecd53983e1f
Diego Vega <diegovega@me.com>
2023-08-03 14:10:15 +0200

    Add examples

--- src/TreeTraversal.java
+++ src/TreeTraversal.java
@@ -5,5 +5,23 @@ public class TreeTraversal {
     * tree structure. We need to implement a function that takes the root of a binary tree and returns a list of its
     * nodes' values in an inorder traversal.
     *
+    * Example 1:
+    *       Input:
+    *     1
+    *      \
+    *       2
+    *      /
+    *     3
+    *   Output: [1,3,2]
+    *
+    * Example 2:
+    *       Input:
+    *         5
+    *        / \
+    *       3   6
+    *      / \
+    *     2   4
+    *
+    *   Output: [2,3,4,5,6]
     * */
 }
