0058262b546c1c84016dc4c24c740040ea107e28
Diego Vega <diegovega@me.com>
2023-08-03 17:50:28 +0200

    Add problem explanation

---
+++ src/TreeTraversal2.java
@@ -0,0 +1,11 @@
+public class TreeTraversal2 {
+
+    /*
+    * As a data-driven startup, we aim to provide market analysis to our clients. We are currently building a binary
+    * tree to store our client data. However, we have noticed that traversing the tree in a zigzag order would allow
+    * us to better analyze the data. We need a function that takes the root of our binary tree and returns a zigzag
+    * order traversal of its nodes' values. This means that the traversal should go from left to right for the first
+    * level, then from right to left for the second level, and so on.
+    *
+     * */
+}
