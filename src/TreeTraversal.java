5ee649c2c09ed945f54d9da23696b9212157176b
Diego Vega <diegovega@me.com>
2023-08-03 14:07:42 +0200

    Add problem explanation

---
+++ src/TreeTraversal.java
@@ -0,0 +1,9 @@
+public class TreeTraversal {
+
+    /*
+    * As a startup, we are developing a new feature for our web application that requires us to process data in a binary
+    * tree structure. We need to implement a function that takes the root of a binary tree and returns a list of its
+    * nodes' values in an inorder traversal.
+    *
+    * */
+}
