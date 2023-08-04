5659e11f27737f62b82959bb3b5c1bbe40dd030b
Diego Vega <diegovega@me.com>
2023-08-04 11:02:33 +0200

    Add problem explanation

---
+++ src/ConstructBinaryTree.java
@@ -0,0 +1,12 @@
+public class ConstructBinaryTree {
+
+    /*
+    * We are building a new product that relies on a binary tree data structure, and we need to be able to construct
+    * the tree given its preorder and inorder traversals. We have two integer arrays, one representing the preorder
+    * traversal (root, left subtree, right subtree) and the other representing the inorder traversal (left subtree,
+    * root, right subtree). Can you help us implement a function that takes in these two arrays and returns the binary
+    * tree?
+    *
+    *
+     * */
+}
