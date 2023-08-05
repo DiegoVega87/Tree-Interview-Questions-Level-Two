03bf2b71cad2579f81a09f5b841497663ffb9de5
Diego Vega <diegovega@me.com>
2023-08-05 16:31:40 +0200

    Add problem explanation

---
+++ src/FindingKthSmallest.java
@@ -0,0 +1,11 @@
+public class FindingKthSmallest {
+
+    /*
+    * As startup in the e-commerce industry, we have a database of all our products organized in a binary search tree.
+    * We need to find the kth smallest product in our database to display it on the homepage. We need you to write a
+    * function that takes in the root of our binary search tree and an integer k and returns the kth smallest product
+    * in the tree.
+    *
+    *
+    * */
+}
