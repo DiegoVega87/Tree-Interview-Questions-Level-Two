eecc7c832d52df8cee7f3080d75473b49301ecbd
Diego Vega <diegovega@me.com>
2023-08-05 16:34:23 +0200

    Add examples

--- src/FindingKthSmallest.java
+++ src/FindingKthSmallest.java
@@ -6,6 +6,28 @@ public class FindingKthSmallest {
     * function that takes in the root of our binary search tree and an integer k and returns the kth smallest product
     * in the tree.
     *
+    * Example 1:
+    *           Input:
+    *           5
+    *          / \
+    *         3   7
+    *        / \   \
+    *       2   4   8
+    * k = 3
+    * Output: 4
+    * Explanation: The 3rd smallest product in the tree is 4.
     *
+    *
+    * Example 2:
+    *           Input:
+    *           10
+    *          / \
+    *         5   15
+    *        / \    \
+    *       3   7    18
+    *
+    * k = 6
+    * Output: 18
+    * Explanation: The 6th smallest product in the tree is 18.
     * */
 }
